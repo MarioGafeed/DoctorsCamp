21ef529a71a1cb4eb4d21ba8069bb3dc6618766c
MarioGafeed <jesushostingme@gmail.com>
2022-06-02 05:17:38 +0200

    fix Tag

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,30 +15,50 @@ class PostResource extends JsonResource
       if ($tagNames->toArray() != null) {
         foreach ($tagNames as $tagName) {
           $tag =  \Spatie\Tags\Tag::where('slug->en', $tagName)->first();
-          dd($tag);
+          dd($tagName) ;      
             if ($tag != null) {
               $tagPosts = \App\Models\Post::withAnyTags([$tag])->get();
+              return [
+                  'id'           => $this->id,
+                  'title'        => $this['title_' . request()->header('accept-language', 'en')],
+                  'keyword'      => $this->keyword,
+                  'content'      => json_decode($this->content, true)[request()->header('Accept-Language', 'ar')] ?? null,
+                  'description'  => json_decode($this->desc, true)[request()->header('Accept-Language', 'ar')] ?? null,
+                  'user_name'    => $this->user->name,
+                  'category_id'  => $this->category->id,
+                  'category_name'=> $this->category['title_' . request()->header('accept-language', 'en')],
+                  // 'youtubeURL'   => $this->youtubeURL,
+                  'image'        => $this->getFirstMediaUrl(),
+                  'likes_count'  => $this->likes()->count(),
+                  'user_like?'   => $this->liked($this->user->id),
+                  'comments_count' => $this->comments()->count(),
+                  'comments'     => CommentResource::collection($postComments),
+                  'tag_posts'    => TagpostsResource::collection($tagPosts),
+                  // 'comments'     => $this->comments()->approved()->select('id', 'comment', 'user_id','updated_at')->get(),
+                  // 'comment_author' => $this->comments()->approved()->pivot->user->name,
+                  'updated_at'   => (string) $this->updated_at,
+              ];
+            }else {
+              return [
+                  'id'           => $this->id,
+                  'title'        => $this['title_' . request()->header('accept-language', 'en')],
+                  'keyword'      => $this->keyword,
+                  'content'      => json_decode($this->content, true)[request()->header('Accept-Language', 'ar')] ?? null,
+                  'description'  => json_decode($this->desc, true)[request()->header('Accept-Language', 'ar')] ?? null,
+                  'user_name'    => $this->user->name,
+                  'category_id'  => $this->category->id,
+                  'category_name'=> $this->category['title_' . request()->header('accept-language', 'en')],
+                  // 'youtubeURL'   => $this->youtubeURL,
+                  'image'        => $this->getFirstMediaUrl(),
+                  'likes_count'  => $this->likes()->count(),
+                  'user_like?'   => $this->liked($this->user->id),
+                  'comments_count' => $this->comments()->count(),
+                  'comments'     => CommentResource::collection($postComments),
+                  // 'comments'     => $this->comments()->approved()->select('id', 'comment', 'user_id','updated_at')->get(),
+                  // 'comment_author' => $this->comments()->approved()->pivot->user->name,
+                  'updated_at'   => (string) $this->updated_at,
+              ];
             }
-          return [
-              'id'           => $this->id,
-              'title'        => $this['title_' . request()->header('accept-language', 'en')],
-              'keyword'      => $this->keyword,
-              'content'      => json_decode($this->content, true)[request()->header('Accept-Language', 'ar')] ?? null,
-              'description'  => json_decode($this->desc, true)[request()->header('Accept-Language', 'ar')] ?? null,
-              'user_name'    => $this->user->name,
-              'category_id'  => $this->category->id,
-              'category_name'=> $this->category['title_' . request()->header('accept-language', 'en')],
-              // 'youtubeURL'   => $this->youtubeURL,
-              'image'        => $this->getFirstMediaUrl(),
-              'likes_count'  => $this->likes()->count(),
-              'user_like?'   => $this->liked($this->user->id),
-              'comments_count' => $this->comments()->count(),
-              'comments'     => CommentResource::collection($postComments),
-              'tag_posts'    => TagpostsResource::collection($tagPosts),
-              // 'comments'     => $this->comments()->approved()->select('id', 'comment', 'user_id','updated_at')->get(),
-              // 'comment_author' => $this->comments()->approved()->pivot->user->name,
-              'updated_at'   => (string) $this->updated_at,
-          ];
          }
        }else {
          return [
